99d997f194b48716904d77891819f62be899f558
Jakub Misina <109918766+jmisina@users.noreply.github.com>
2024-03-25 18:36:10 +0100

    Update main.py
    english translation

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import time
 from datetime import datetime
 
 def copy_files(src_dir, dst_dir):
-    # Tworzenie nowego folderu z aktualnym czasem jako nazwą
+    # New folder creation with current date and time as a folder name
     new_folder = os.path.join(dst_dir, datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
     os.makedirs(new_folder, exist_ok=True)
 
-    # Kopiowanie plików
+    # Copying files
     for file_name in os.listdir(src_dir):
         src_file = os.path.join(src_dir, file_name)
         if os.path.isfile(src_file):
             shutil.copy(src_file, new_folder)
 
-    # Usuwanie najstarszych folderów, jeśli ich liczba przekracza 5
+    # Deleting eldest files if their number is over 5 
     all_folders = [os.path.join(dst_dir, f) for f in os.listdir(dst_dir) if os.path.isdir(os.path.join(dst_dir, f))]
     all_folders.sort(key=os.path.getmtime)
     while len(all_folders) > 5:
@@ -23,15 +23,14 @@ def copy_files(src_dir, dst_dir):
         shutil.rmtree(oldest_folder)
     print(str(datetime.now().strftime('%Y-%m-%d_%H-%M-%S')) + " backup done")
 
-# Ścieżki do folderów źródłowego i docelowego
+# Directory paths
+dst_dir = '' # Path for directory for files to be copied into
+src_dir = '' # Path for directory from which the files should be copied from
 
-dst_dir = 'C:\\Users\\qwedr\\Desktop\\bekap'
-src_dir = 'C:\\Users\\qwedr\\AppData\\Roaming\\EldenRing\\76561198086944617'
-
-# Planowanie zadania co 10 minut
+# Scheduling the task every ten minutes
 schedule.every(10).minutes.do(copy_files, src_dir, dst_dir)
 
-# Pętla, która uruchamia zaplanowane zadania
+# Main loop
 while True:
     schedule.run_pending()
     time.sleep(1)
